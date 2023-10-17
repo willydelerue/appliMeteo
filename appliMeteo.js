aec520bb738f2036ecbd3fa66ad4075d0b1d27ae
willydelerue <willy.delerue@gmail.com>
2023-10-17 13:56:25 +0200

    Step 1

---
+++ appliMeteo.js
@@ -0,0 +1,6 @@
+let ville = "Paris";
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=da7b3605119c1f7d42a31bc148149844&units=metric';
+
+let request = new XMLHttpRequest();
+
