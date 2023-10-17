0ae8460000d093712d42ec8e86fa053d0fc39298
willydelerue <willy.delerue@gmail.com>
2023-10-17 14:35:39 +0200

    step 3

--- appliMeteo.js
+++ appliMeteo.js
@@ -2,7 +2,7 @@ let ville = "Paris";
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=da7b3605119c1f7d42a31bc148149844&units=metric';
 
-let request = new XMLHttpRequest();
+let requete = new XMLHttpRequest();
 
 requete.open('GET', url);
 requete.responseType = 'json';
@@ -12,6 +12,11 @@ requete.onload = function() {
     if (requete.readyState === XMLHttpRequest.DONE) {
         if (requete.status === 200) {
         let reponse     = requete.response // On stocke la réponse
+        let temperature = reponse.main.temp;
+        let ville       = reponse.name;
+
+        document.querySelector('#temperature_label').textContent = temperature;
+        document.querySelector('#ville').textContent = ville;
         console.log(reponse);
         }
     }
